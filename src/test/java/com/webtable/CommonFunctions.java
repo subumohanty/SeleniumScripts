47e2b762fa370a9c364266e874dab1e80e81872f
subumohanty <mohantysubhasis16@gmail.com>
2023-06-17 21:45:23 +0530

    Initiated fresh github repository for selenium scripts

--- src/test/java/com/webtable/CommonFunctions.java
+++ src/test/java/com/webtable/CommonFunctions.java
@@ -1,5 +1,54 @@
 package com.webtable;
 
-public class CommonFunctions {
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
 
+public class CommonFunctions {
+		
+	WebDriver driver = null;
+	
+	public CommonFunctions() {
+		//TODO Auto-generated constructor stub
+		System.setProperty("webDriver.chrome.driver", "./executables/chromedriver.exe");
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
+	}
+	/**
+	 * <p>Navigates to specifiied url</p>
+	 * @param url
+	 */
+	public void navigate(String url) {
+		driver.get(url);
+	}
+	/**
+	 * Clicks on provided webElement
+	 * @param elem
+	 */
+	
+	public void clickElement(WebElement elem) {
+		elem.click();
+	}
+	/**
+	 * <p>Fetches content within a webElement
+	 * @param elem
+	 */
+	public String getElementText(WebElement elem) {
+		return elem.getText();
+	}
+	
+	public String getElementText(String xpath) {
+		return driver.findElement(By.xpath(xpath)).getText();
+	}
+	
+	public int getCount(String xpath) {
+		return driver.findElements(By.xpath(xpath)).size();
+	}
+	
+	public void closeBrowser() {
+		driver.close();
+		driver.quit();
+		
+	}
 }
--- src/test/java/com/webtable/Script.java
+++ src/test/java/com/webtable/Script.java
@@ -1,5 +1,17 @@
 package com.webtable;
 
 public class Script {
-
+	public static void main(String[] args) {
+		CommonFunctions comm = new CommonFunctions();
+		comm.navigate("https://qavbox.github.io/demo/webtable/");
+		int rowCount = comm.getCount("//table[@id=''table02']/tbody/tr");
+		for(int i=1; i<rowCount; i++) {
+			int colCount = comm.getCount("//table[@id=''table02']/tbody/tr["+i+"]/td");
+			for(int j=1; j<colCount; j++) {
+				String data = comm.getElementText("//table[@id=''table02']/tbody/tr[\"+i+\"]/td/["+j+"]");
+				System.out.println(data);
+			}
+		}
+		comm.closeBrowser();
+	}
 }
